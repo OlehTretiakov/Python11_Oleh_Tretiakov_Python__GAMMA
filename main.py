4f17bc069e7f2d99969b6e1cea6ed6e677eb2700
Oleh Tretiakov <tretiakov.olek@gmail.com>
2023-02-02 13:24:52 +0200

    not actual

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import datetime
 
 
-    
+
+def get_to_past(n):
+    today = datetime.datetime.fromtimestamp(n)
+    two_weeks_ago = today - datetime.timedelta(weeks=2)
+    return two_weeks_ago
+n = float(input())
+print(get_to_past(n))
+
